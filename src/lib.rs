1ae7e76e4b06b203f5b5f2cf45297fd4616788c2
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-02-17 12:02:31 +0100

    feat(add lints): `missing_docs` + `rust_2018`

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
+#![warn(missing_docs, rust_2018_idioms)]
+
 use crate::error::{Error, ErrorKind};
 use std::path::PathBuf;
 

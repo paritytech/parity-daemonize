0f731f54419fbdfcda37b86e6ff48087ab0147d7
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-02-17 13:11:20 +0100

    fix(lint): `rust_2018_idioms`

--- src/error.rs
+++ src/error.rs
@@ -80,7 +80,7 @@ pub enum ErrorKind {
 }
 
 impl Fail for Error {
-	fn cause(&self) -> Option<&Fail> {
+	fn cause(&self) -> Option<&dyn Fail> {
 		self.inner.cause()
 	}
 
@@ -90,7 +90,7 @@ impl Fail for Error {
 }
 
 impl Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		Display::fmt(&self.inner, f)
 	}
 }
